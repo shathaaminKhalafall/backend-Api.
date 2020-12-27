dd786e9fbf13a0a805465f58a4237777741e696e
shathaaminKhalafall <72312523+shathaaminKhalafall@users.noreply.github.com>
2020-12-27 16:10:28 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,6 +1,4 @@
-
-//edit your code 
-fetch('http://localhost:3000/user',{
+fetch('http://localhost:3000/user?name="shatha"',{
     method:'GET',
     headers: {
         'Content-Type': 'application/json',
@@ -10,8 +8,21 @@ fetch('http://localhost:3000/user',{
     console.log(err);
 })
 
-fetch('http://localhost:3000/user',{
-    method:'post',
+
+
+fetch('http://localhost:3000/user/nariman',{
+    method:'POST',
+    headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({massage: "hello everyone", name: 'shatha'}),
+}).then(res=>res.json()).then(val=>console.log(val))
+.catch(err=>{
+    console.log(err);
+})
+
+fetch('http://localhost:3000/user/nariman',{
+    method:'GET',
     headers: {
         'Content-Type': 'application/json',
       },
